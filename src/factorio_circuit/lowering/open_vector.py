a32d5cdc3e7e8b6f6f7541f4735f8233de103414
nn971 <60208623+nn971@users.noreply.github.com>
2026-08-14 01:44:35 +0800

    feat: extend whole-vector realization

--- src/factorio_circuit/lowering/open_vector.py
+++ src/factorio_circuit/lowering/open_vector.py
@@ -1,1 +1,26 @@
-"""Whole-vector lowering extension."""
+"""Whole-vector realization extension for the standard abstract lowerer."""
+
+from factorio_circuit.frontend import _VectorBinaryOp, _VectorFilter, _VectorScalarOp
+from factorio_circuit.ir.semantic import VectorValue
+from factorio_circuit.lowering.ir_to_abstract_physical import AbstractPhysicalLowerer, RealizedVector
+
+from .vector_binary import realize_vector_binary
+from .vector_unary import realize_vector_filter, realize_vector_scalar
+
+
+class VectorLowerer(AbstractPhysicalLowerer):
+    def realize_vector(self, value: VectorValue) -> RealizedVector:
+        item: object = value
+        cached = self.vector_memo.get(id(item))
+        if cached is not None:
+            return cached
+        if isinstance(item, _VectorBinaryOp):
+            result = realize_vector_binary(self, item)
+        elif isinstance(item, _VectorScalarOp):
+            result = realize_vector_scalar(self, item)
+        elif isinstance(item, _VectorFilter):
+            result = realize_vector_filter(self, item)
+        else:
+            return super().realize_vector(value)
+        self.vector_memo[id(item)] = result
+        return result
