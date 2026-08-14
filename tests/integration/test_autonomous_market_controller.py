e15b0a17c3d25f72c399147f55d900d92bb97fa0
nn971 <60208623+nn971@users.noreply.github.com>
2026-08-14 20:07:08 +0800

    test(market): cover reader settling diagnostics

--- tests/integration/test_autonomous_market_controller.py
+++ tests/integration/test_autonomous_market_controller.py
@@ -41,4 +41,6 @@ def test_market_controller_is_composed_only_from_primitive_freeze_registers(opti
         "mode",
         "top_target",
         "blocked_on_full_stack",
+        "debug_reader_ingredients",
+        "debug_ingredient_missing",
     }
