bc2b9a21a5aad120bdc75a32dd5bcf54498b284d
nn971 <60208623+nn971@users.noreply.github.com>
2026-08-16 20:51:10 +0800

    export lamp screen generator

--- src/factorio_circuit/devices/__init__.py
+++ src/factorio_circuit/devices/__init__.py
@@ -1,5 +1,14 @@
 """Reusable generators for fixed external Factorio devices."""
 
+from factorio_circuit.devices.lamp_screen import (
+    PIXEL_SIGNALS,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+    build_lamp_screen_blueprint,
+    generate_lamp_screen_blueprint_string,
+    pixel_signal,
+    rgb,
+)
 from factorio_circuit.devices.player_movement_detector import (
     DIRECTION_ORDER,
     DIRECTION_SIGNALS,
@@ -10,6 +19,13 @@ from factorio_circuit.devices.player_movement_detector import (
 __all__ = [
     "DIRECTION_ORDER",
     "DIRECTION_SIGNALS",
+    "PIXEL_SIGNALS",
+    "SCREEN_HEIGHT",
+    "SCREEN_WIDTH",
+    "build_lamp_screen_blueprint",
     "build_player_movement_detector_blueprint",
+    "generate_lamp_screen_blueprint_string",
     "generate_player_movement_detector_blueprint_string",
+    "pixel_signal",
+    "rgb",
 ]
