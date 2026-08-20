57f7986571113b78e7a0f54f49ce46001930db2a
nn971 <60208623+nn971@users.noreply.github.com>
2026-08-20 14:35:53 +0800

    Export reusable module interface synthesis API

--- src/factorio_circuit/synthesis/__init__.py
+++ src/factorio_circuit/synthesis/__init__.py
@@ -1,13 +1,25 @@
 """Physical synthesis from abstract target circuits to final layouts."""
 
+from .interface import (
+    ModuleInterface,
+    compile_module,
+    encode_blueprint_payload,
+    placement_for_interface,
+    resolve_interface_anchors,
+)
 from .layout import Layout
 from .physical import synthesize_layout
 from .placement import PlacementMetrics, PlacementOptions, placement_metrics
 
 __all__ = [
     "Layout",
+    "ModuleInterface",
     "PlacementMetrics",
     "PlacementOptions",
+    "compile_module",
+    "encode_blueprint_payload",
+    "placement_for_interface",
     "placement_metrics",
+    "resolve_interface_anchors",
     "synthesize_layout",
 ]
