f253b3a173fa35ba41c76c4ecacd12493c53e724
nn971 <60208623+nn971@users.noreply.github.com>
2026-08-22 19:43:53 +0800

    Fix mapped lowerer regression import

--- tests/mapping/test_state_lower_noop_alignment.py
+++ tests/mapping/test_state_lower_noop_alignment.py
@@ -1,9 +1,9 @@
 from factorio_circuit.lowering.ir_to_abstract_physical import RealizedValue, RealizedVector
-from factorio_circuit.mapping.state_lower_entry import _NoOpSafeMappedPeriodicStateLowerer
+from factorio_circuit.mapping.state_lower_entry import _BoundarySafeMappedPeriodicStateLowerer
 
 
 def test_scalar_same_phase_alignment_does_not_consume_planned_delivery() -> None:
-    lowerer = object.__new__(_NoOpSafeMappedPeriodicStateLowerer)
+    lowerer = object.__new__(_BoundarySafeMappedPeriodicStateLowerer)
     sentinel = object()
     lowerer.delivery_queues = {(7, 11): [sentinel]}
     lowerer.scalar_origin = {(3, 5): 7}
@@ -16,7 +16,7 @@ def test_scalar_same_phase_alignment_does_not_consume_planned_delivery() -> None
 
 
 def test_vector_same_phase_alignment_does_not_consume_planned_delivery() -> None:
-    lowerer = object.__new__(_NoOpSafeMappedPeriodicStateLowerer)
+    lowerer = object.__new__(_BoundarySafeMappedPeriodicStateLowerer)
     sentinel = object()
     lowerer.delivery_queues = {(9, 13): [sentinel]}
     lowerer.vector_origin = {4: 9}
