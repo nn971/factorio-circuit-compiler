3d155aba518ec07a34b253c345e578725fac04af
nn971 <60208623+nn971@users.noreply.github.com>
2026-08-23 14:47:34 +0800

    Restore generator acceptance to opt-in suite

--- tests/examples/autonomous_mall/test_worker_pool_generator_acceptance_probe.py
+++
@@ -1,7 +0,0 @@
-from examples.autonomous_mall.worker_pool import build_worker_pool_probe_blueprint
-
-
-def test_two_worker_generator_end_to_end() -> None:
-    blueprint = build_worker_pool_probe_blueprint(2)
-    entities = blueprint["entities"]
-    assert sum(entity.get("name") == "assembling-machine-3" for entity in entities) == 2
