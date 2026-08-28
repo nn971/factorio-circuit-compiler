c9edcc2ed4b5b9d544b18ed5ae9961839e569cdd
nn971 <60208623+nn971@users.noreply.github.com>
2026-08-28 22:24:45 +0800

    Export component geometry optimization API

--- src/factorio_circuit/synthesis/__init__.py
+++ src/factorio_circuit/synthesis/__init__.py
@@ -10,10 +10,23 @@ from .layout_optimizer import (
     physical_layout_metrics,
     validate_physical_layout,
 )
+from .component_geometry import (
+    ComponentAccessPoint,
+    ComponentLayoutOptimizationProblem,
+    ComponentRegion,
+    RigidComponentConstraint,
+    RigidComponentMember,
+    lower_component_layout_problem,
+    optimize_component_layout,
+    validate_component_layout_problem,
+)
 from .physical import synthesize_layout
 from .placement import PlacementMetrics, PlacementOptions, placement_metrics
 
 __all__ = [
+    "ComponentAccessPoint",
+    "ComponentLayoutOptimizationProblem",
+    "ComponentRegion",
     "Layout",
     "LayoutOptimizationProblem",
     "LayoutOptimizationResult",
@@ -21,9 +34,14 @@ __all__ = [
     "PlacementMetrics",
     "PlacementOptions",
     "PhysicalLayoutMetrics",
+    "RigidComponentConstraint",
+    "RigidComponentMember",
+    "lower_component_layout_problem",
+    "optimize_component_layout",
     "optimize_physical_layout",
     "placement_metrics",
     "physical_layout_metrics",
     "synthesize_layout",
+    "validate_component_layout_problem",
     "validate_physical_layout",
 ]
